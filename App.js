7ba691cdaa73d958a4314e9e7c042b9af0dce57c
kwonbi <kwonbi@pumpkinev.co.kr>
2023-09-26 11:42:06 +0900

    수정

--- App.js
+++ App.js
@@ -36,10 +36,10 @@ const FriendList = () => {
       <Profile name="녜냥" uri="https://raw.githubusercontent.com/bi-sz/friendsList/master/image/Neow.jpg" />
       <Profile name="미성" uri="https://raw.githubusercontent.com/bi-sz/friendsList/master/image/Miseong.png" />
       <Profile name="박재형" uri="https://raw.githubusercontent.com/bi-sz/friendsList/master/image/Jaehyung.png" />
-      <Profile name="박승수" uri="" />
+      <Profile name="박승수" uri="https://raw.githubusercontent.com/bi-sz/friendsList/master/image/seungsoo.jpg" />
       <Profile name="송츄" uri="https://raw.githubusercontent.com/bi-sz/friendsList/master/image/sonhChu.png" />
       <Profile name="정민" uri="" />
-      <Profile name="채붕이" uri="" />
+      <Profile name="채붕이" uri="https://raw.githubusercontent.com/bi-sz/friendsList/master/image/Chaewon.jpg" />
       <Profile name="호진" uri="" />
     </View>
   );
