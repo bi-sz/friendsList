41610b49cf4ea05e1981beae9cf19a0d140eea0e
kwonbi <kwonbi@pumpkinev.co.kr>
2023-09-26 11:33:55 +0900

    수정

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
+import React from "react";
 import { StyleSheet, Text, View } from 'react-native';
+
+const Header = (props) => {
+  console.log('props of Header', props);
+  return <Text>{props.title}</Text>
+}
+const MyProfile = () => {
+  return <Text>MyProfile</Text>
+}
+const Division = () => {
+  return <Text>Division</Text>
+}
+const FriendSection = () => {
+  return <Text>FriendSection</Text>
+}
+const FriendList = () => {
+  return (
+    <View>
+      <Text>미성</Text>
+      <Text>송이</Text>
+      <Text>재형</Text>
+      <Text>승수</Text>
+      <Text>채원</Text>
+      <Text>혜정</Text>
+      <Text>현석</Text>
+      <Text>호진</Text>
+    </View>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header title="친구"/>
+      <MyProfile />
+      <Division />
+      <FriendSection />
+      <FriendList />
     </View>
   );
 }
